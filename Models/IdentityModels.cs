754e73bedd9177e951a9dbc66844265f081aa963
Diego Olmedo <DO166666@fi365.ort.edu.uy>
2021-01-22 22:00:24 -0300

    Se agrega método Seed para dbContext. Ahora cada desarrollador tiene un User Administrador y otro Cliente desde que se crea la bd.

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -55,9 +55,45 @@ namespace FlipWeb.Models
         public DbSet<Contacto> Contactos { get; set; }
         public DbSet<Reporte> Reportes { get; set; }
 
+        public class FliplDBInitializer : CreateDatabaseIfNotExists<ApplicationDbContext>
+        {
+            protected override void Seed(ApplicationDbContext context)
+            {
+                var roleStore = new RoleStore<IdentityRole>(context);
+                var roleManager = new RoleManager<IdentityRole>(roleStore);
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                roleManager.Create(new IdentityRole("Administrador"));
+                roleManager.Create(new IdentityRole("Cliente"));
+
+                //Admin Diego
+                ApplicationUser Admin1 = new ApplicationUser { Email = "olmedodiego10@gmail.com", Nombre = "Diego", Apellido = "Olmedo", Cedula = "45296002", Celular = "099593771", RolString = "Administrador", Telefono = "22279539", Premium = true, UserName = "olmedodiego10@gmail.com" };
+                userManager.Create(Admin1, "Diego10111993");
+                userManager.AddToRole(Admin1.Id, "Administrador");
+
+                //Admin Adrián
+                ApplicationUser Admin2 = new ApplicationUser { Email = "adrianrodriguez0510@gmail.com", Nombre = "Adrián", Apellido = "Rodríguez", Cedula = "11111111", Celular = "099111111", RolString = "Administrador", Telefono = "11111111", Premium = true, UserName = "adrianrodriguez0510@gmail.com" };
+                userManager.Create(Admin2, "Adrian-1899");
+                userManager.AddToRole(Admin2.Id, "Administrador");
+
+                //Cliente Diego
+                ApplicationUser Cliente1 = new ApplicationUser { Email = "olmedodiegocuentas@gmail.com", Nombre = "Diego", Apellido = "Olmedo", Cedula = "45296002", Celular = "099593771", RolString = "Cliente", Telefono = "22279539", Premium = true, UserName = "olmedodiegocuentas@gmail.com" };
+                userManager.Create(Cliente1, "Diego10111993");
+                userManager.AddToRole(Cliente1.Id, "Cliente");
+
+                //Cliente Adrián
+                ApplicationUser Cliente2 = new ApplicationUser { Email = "yiyo0510@hotmail.com", Nombre = "Adrián", Apellido = "Rodríguez", Cedula = "11111111", Celular = "099111111", RolString = "Cliente", Telefono = "099111111", Premium = true, UserName = "yiyo0510@hotmail.com" };
+                userManager.Create(Cliente2, "Adrian-1899");
+                userManager.AddToRole(Cliente2.Id, "Cliente");
+
+                base.Seed(context);
+            }
+        }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+            Database.SetInitializer(new FliplDBInitializer());
         }
 
         public static ApplicationDbContext Create()
